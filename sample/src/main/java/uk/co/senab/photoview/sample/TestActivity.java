7df481b422156537ce1d9674055f3a485c672987
tanglie1993 <tanglie23@163.com>
2016-08-07 13:52:26 +0800

    实现imageview的放缩

---
+++ sample/src/main/java/uk/co/senab/photoview/sample/TestActivity.java
@@ -0,0 +1,67 @@
+package uk.co.senab.photoview.sample;
+
+import android.graphics.Matrix;
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.widget.ImageView;
+
+
+public class TestActivity extends Activity {
+
+    private ScaleGestureDetector detector;
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+        Matrix matrix = new Matrix();
+        imageView = (ImageView) findViewById(R.id.imageView);
+        ScaleGestureDetector.OnScaleGestureListener mScaleListener = new ScaleGestureDetector.OnScaleGestureListener() {
+
+            @Override
+            public boolean onScale(ScaleGestureDetector detector) {
+                float scaleFactor = detector.getScaleFactor();
+
+                if (Float.isNaN(scaleFactor) || Float.isInfinite(scaleFactor))
+                    return false;
+
+                TestActivity.this.onScale(scaleFactor, detector.getFocusX(), detector.getFocusY());
+                return true;
+            }
+
+            @Override
+            public boolean onScaleBegin(ScaleGestureDetector detector) {
+                return true;
+            }
+
+            @Override
+            public void onScaleEnd(ScaleGestureDetector detector) {
+                // NO-OP
+            }
+        };
+        detector = new ScaleGestureDetector(this, mScaleListener);
+        imageView.setImageMatrix(matrix);
+    }
+
+
+    public void onScale(float scaleFactor, float focusX, float focusY) {
+        System.out.println(String.format("onScale: scale: %.2f. fX: %.2f. fY: %.2f",
+                scaleFactor, focusX, focusY));
+//        matrix.postScale(scaleFactor, scaleFactor, focusX, focusY);
+        Matrix matrix = new Matrix (imageView.getImageMatrix());
+        matrix.postScale(scaleFactor, scaleFactor, focusX, focusY);
+        imageView.setImageMatrix(matrix);
+        imageView.invalidate();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        detector.onTouchEvent(ev);
+        return super.onTouchEvent(ev);
+    }
+}
